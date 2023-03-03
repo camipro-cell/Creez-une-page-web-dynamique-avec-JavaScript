427ad7406922c44097f3685680244f65576804df
Camille Jacquet <cami.c.pro@gmail.com>
2023-03-03 17:51:08 +0100

    Création de class: index.js et addeventlistener

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -13,6 +13,7 @@ fetch("http://localhost:5678/api/works")
 		//console.log(work);
 		// <figure>
 		let myFigure = document.createElement('figure');
+		myFigure.setAttribute ('class','work-item category-id-?');
 
 		// <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
 		let myImg = document.createElement('img');
@@ -27,6 +28,9 @@ fetch("http://localhost:5678/api/works")
 
 		// Adding the new <figure> into the existing div.gallery
 		document.querySelector("div.gallery").appendChild(myFigure);
+		
+	    
+
 	});
 })
 .catch(function(err) {
@@ -44,20 +48,43 @@ fetch("http://localhost:5678/api/categories")
 	let categories = data;
 	console.log(categories);
 	// Looping on each work
+	
 	categories.forEach((category, index) => {
 		//console.log(category);
-
+    
 		// <button>
 		let myButton = document.createElement('button');
+		myButton.setAttribute('class', 'work-filter buttons-design');
+		myButton.setAttribute ('data-filter', 'category-id-?');
+		
 		myButton.textContent = category.name;
-
+		
 		// Adding the new <button> into the existing div.filters
 		document.querySelector("div.filters").appendChild(myButton);
-	});
-})
+		
+	    myButton.addEventListener('click', function(event) {
+			console.log(event.target);
+			this.getAttribute('data-filter');
+			if (myFigure === myButton.getAttribute)
+			{ myFigure.setAttribute('style', 'display: block;')}
+			else {
+				myFigure.setAttribute('style', 'display: none;')
+			}
+		} );
+        
+			
+		});
+		
+	})
+
 .catch(function(err) {
 	console.log(err);
 });
+
+
+
+
+
 
 
 
