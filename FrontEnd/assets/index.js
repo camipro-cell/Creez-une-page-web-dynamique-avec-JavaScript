476415cfe7c7b7c67de37f4fe971833071ba3cc5
Camille Jacquet <cami.c.pro@gmail.com>
2023-03-22 17:32:28 +0100

    Modif input file et formulaire post avec fetch

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -79,16 +79,6 @@ fetch("http://localhost:5678/api/categories")
 	console.log(err);
 });
 
-
-
-
-
-
-
-
-
-
-
 // New fetch to see all works in the modal
 fetch("http://localhost:5678/api/works")
 .then(function(response) {
@@ -146,13 +136,23 @@ fetch("http://localhost:5678/api/works")
 					break;
 					case 200:
 					case 204:
-						console.log("Supprimer le projet");
-						delete confirm("Supression réussi!");
 						
-						// @todo
-						// Supprimer le work-item dans la popup
-						// Supprimer le work-item dans la page
-						// Travail préliminaire : A la création ajouter un id="work-item-X" (et work-popup-item-X) à chacun des work-items
+						console.log("Supprimer le projet");
+						delete confirm ("Suppresion réussi!")
+						document.querySelectorAll(`#work-item-${work.id}`).forEach(workItem => {
+							workItem.parentNode.removeChild(workItem);
+							
+						});
+						console.log(`work-item-${work.id}`);
+						
+					
+						document.querySelectorAll(`#work-item-popup-${work.id}`).forEach(workItemPopup => {
+							workItemPopup.parentNode.removeChild(workItemPopup);
+							
+						});
+						console.log(`work-item-popup-${work.id}`);
+					
+						
 					break;
 					default:
 						alert("Erreur inconnue");
@@ -212,7 +212,37 @@ document.addEventListener('DOMContentLoaded', function() {
 		let modalWorks = document.getElementById("modal-works");
 		modalWorks.style.display = "block";
 	});
-
+	
+	// close both modal windows with a click outside
+	document.querySelectorAll('#modal-works').forEach(modalWorks => {
+		modalWorks.addEventListener('click', function(event) {
+			event.stopPropagation();
+			console.log(event);
+		})
+	
+		document.querySelectorAll('#modal-edit').forEach(modalEdit => {
+			modalEdit.addEventListener('click', function(event) {
+				event.stopPropagation();
+				console.log(event);
+		})
+	
+			document.getElementById("modal").addEventListener('click', function(event) {
+				event.preventDefault();
+				console.log(event);
+				let modal = document.getElementById("modal");
+				modal.style.display = "none";
+				let modalWorks = document.getElementById("modal-works");
+		 		modalWorks.style.display = "none";
+				let modalEdit = document.getElementById("modal-edit");
+				modalEdit.style.display = "none";
+				// Reset form
+				document.getElementById('modal-edit-work-form').reset();
+				document.getElementById("submit-new-work").disabled = false;
+				document.getElementById("submit-new-work").style.backgroundColor= "#A7A7A7";
+			});
+		});
+	});
+	
 	// Close first window of modal with button "x"
 	document.getElementById('button-to-close-first-window').addEventListener('click', function(event) {
 		event.preventDefault();
@@ -231,6 +261,10 @@ document.addEventListener('DOMContentLoaded', function() {
 		modal.style.display = "none";
 		let modalEdit = document.getElementById("modal-edit");
 		modalEdit.style.display = "none";
+		// Reset form
+		document.getElementById('modal-edit-work-form').reset();
+		document.getElementById("submit-new-work").disabled = false;
+		document.getElementById("submit-new-work").style.backgroundColor= "#A7A7A7";
 	});
 
 	// Open second window of modal with button "Ajouter photo"
@@ -251,32 +285,153 @@ document.addEventListener('DOMContentLoaded', function() {
 		modalWorks.style.display = "block";
 		let modalEdit = document.getElementById("modal-edit");
 		modalEdit.style.display = "none";
+		// Reset form
+		document.getElementById('modal-edit-work-form').reset();
+		document.getElementById("submit-new-work").disabled = false;
+		document.getElementById("submit-new-work").style.backgroundColor= "#A7A7A7";
 	});
 
-	// Handling form
-	document.getElementById('modal-edit-work-form').addEventListener('submit', function(event) {
-		event.preventDefault();
-		let formData = new FormData();
-		formData.append('title', document.getElementById('form-image').value);
-		formData.append('category', document.getElementById('form-image').value);
-		formData.append('image', document.getElementById('form-image').files[0]);
+	
+	
+	function showPreview(event) {
+		if(event.target.files.lenght > 0) {
+			let src = URL.createObjectURL(event.target.files[0]);
+			let preview = document.getElementById("form-image");
+			preview.src = src;
+			preview.style.display = "block";
+		}
+	}
+	
+		// Binder the checkNewProjectFields() function on the 3 fields by listening to the "input" & "change" events
+			document.getElementById('form-title').addEventListener('input', checkNewProjectFields);
+			document.getElementById('form-title').addEventListener('change', checkNewProjectFields);
+			document.getElementById('form-category').addEventListener('input', checkNewProjectFields);
+			document.getElementById('form-category').addEventListener('change', checkNewProjectFields);
+			document.getElementById('form-image').addEventListener('input', checkNewProjectFields);
+			document.getElementById('form-image').addEventListener('change', checkNewProjectFields);
+			//document.getElementById('form-image').addEventListener('input', showPreview);
+			//document.getElementById('form-image').addEventListener('change', showPreview);
+
+		// Check the size of the image file
+			document.getElementById('form-image').addEventListener('change', () => {
+				let file = document.getElementById('form-image').files[0];
+				const maxFileSize = 4 * 1024 * 1024; // 4MB
+				if (file.size > maxFileSize) {
+				  alert("Le fichier sélectionné est trop volumineux. La taille maximale est de 4 Mo.");
+				  document.getElementById('form-image').value = ''; 
+				}
+			  });
+		
+		// Creation of the checkNewProjectFields() function that checks the image + title + category fields
+			function checkNewProjectFields(){
+				let title = document.getElementById('form-title');
+				let category = document.getElementById('form-category');
+				let image = document.getElementById('form-image');
+				let submitWork = document.getElementById("submit-new-work");
+			if(title.value === "" || category.value === "" || image.value === "") {
+				submitWork.disabled = true;
+				submitWork.style.backgroundColor= "#A7A7A7";
+				
+			} else {
+				submitWork.disabled = false;
+				submitWork.style.backgroundColor= "#1D6154";
+			} };
+
+		// Handling form
+			document.getElementById("modal-edit-work-form").addEventListener('submit', function(event) {
+				event.preventDefault();
+				console.log(event);
+				let formData = new FormData();
+				formData.append('title', document.getElementById('form-title').value);
+				formData.append('category', document.getElementById('form-category').value);
+				formData.append('image', document.getElementById('form-image').files[0]);
+		
+
+	
+		
 		fetch('http://localhost:5678/api/works', {
 			method: 'POST',
 			headers: {
-				'Authorization': 'Bearer ' + localStorage.getItem('token')
+				'Authorization': 'Bearer ' + localStorage.getItem('token'),
+				
 			},
 			body: formData
 		})
-		.then(function(response) {
-			if(response.ok) {
-				return response.json();
-			}
-		})
+				.then(function(response) {
+					switch(response.status) {
+						case 500:
+						case 503:
+							alert("Erreur inattendue!");
+						break;
+						case 400:
+						case 404:
+							alert("Impossible d'ajouter le nouveau projet!");
+						break;
+						case 200:
+						case 201:
+							console.log("Projet ajouté avec succés!");
+							return response.json();
+						break;
+						default:
+							alert("Erreur inconnue!");
+						break;
+					}})
+				
+		
 		.then(function(data) {
-			console.log(data);
+	
+		console.log(data);
+		// Close modal
+			let modal = document.getElementById("modal");
+			modal.style.display = "none";
+			let modalEdit = document.getElementById("modal-edit");
+			modalEdit.style.display = "none";
+		// Reset form
+			document.getElementById('modal-edit-work-form').reset();
+			document.getElementById("submit-new-work").disabled = false;
+			document.getElementById("submit-new-work").style.backgroundColor= "#A7A7A7";
+		
+			fetch("http://localhost:5678/api/works") 
+			.then(function(response) {
+				if(response.ok) {
+					return response.json();
+				}
+			})
+			.then(function(data) {
+				
+				console.log(data);
+				let works = data;
+				
+
+				
+					//<figure>
+				works.forEach((work, index=[index.lenght-1] ) => {
+					console.log(index);
+					let myFigure = document.createElement('figure');
+					myFigure.setAttribute ('class', `work-item category-id-0 category-id-${work.categoryId}`);
+					myFigure.setAttribute ('id', `work-item-${work.id}`);
+					// <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
+					let myImg = document.createElement('img');
+					myImg.setAttribute('src', work.imageUrl);
+					myImg.setAttribute('alt', work.title);
+					myFigure.appendChild(myImg);
+					// <figcaption>Abajour Tahina</figcaption>
+					let myFigCaption = document.createElement('figcaption');
+					myFigCaption.textContent = work.title;
+					myFigure.appendChild(myFigCaption);
+					// Adding the new <figure> into the existing div.gallery
+					document.querySelector("div.gallery").appendChild(myFigure);
+			});
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
 		})
-		.catch(function(err) {
-			console.log(err);
-		});
-	});
+			
+			
+	.catch(function(err) {
+	console.log(err);
+});
 })
+});
+
